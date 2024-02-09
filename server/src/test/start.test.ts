c166997ec566d4db06a1dac9deb4fbdf28f8d95d
ebrahim <ebraheem@chalmers.se>
2024-02-09 12:02:22 +0100

    Co-authored-by: pxxch9994 <phongam@chalmers.se>

--- server/src/test/start.test.ts
+++ server/src/test/start.test.ts
@@ -8,20 +8,20 @@ const request = SuperTest.default(app);
 test("End-to-end test", async () => {
     const testname = "Pelle";
 
-    const res1 = await request.post("/user/users").send({name : testname});
+    const res1 = await request.post("/users").send({name : testname, password : 123});
     expect(res1.statusCode).toEqual(201);
     expect(res1.body.name).toEqual(testname);
 
-    const res2 = await request.get("/user/users");
+    const res2 = await request.get("/users");
     expect(res2.statusCode).toEqual(200);
-    expect(res2.body.map((user : User) => user.name)).toContain(testname);
+    expect(res2.body.map((user : User) => user.username)).toContain(testname);
 
 
 });
 
 test("End-to-end test", async () => {
     const testname2 = 444444;
-    const res3 = await request.post("/user/users").send({name : testname2});
+    const res3 = await request.post("users").send({name : testname2, password : 555});
     expect(res3.statusCode).toEqual(400);
 });
 
