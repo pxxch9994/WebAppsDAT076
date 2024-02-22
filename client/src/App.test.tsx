cd5060922645e2943c2b2e85a378e306b24feed8
ebrahim <ebraheem@chalmers.se>
2024-02-22 09:38:21 +0100

    test for frontend.
    Co-authored-by: pxxch9994 <118355635+pxxch9994@users.noreply.github.com>

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Navbar from './components/NavBar';
+import Home from './pages/Home';
+import { Nav } from "react-bootstrap";
+import { MemoryRouter } from 'react-router-dom';
+import NavBar from "./components/NavBar";
+import LoginRegisterSwitch from "./components/LoginRegisterSwitch";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+
+test('renders texts in Home', () => {
+    render(
+        <MemoryRouter>
+            <Home />
+        </MemoryRouter>
+    );
+    screen.debug();
+    const bText = screen.getByText(/Blixten längtar efter att hitta en ny familj!/i);
+    expect(bText).toBeInTheDocument();
+});
+
+test('renders navbar links', () => {
+    render(
+        <MemoryRouter>
+            <NavBar />
+        </MemoryRouter>
+    );
+
+    screen.debug();
+
+    const linkElement = screen.getByText(/Lost & Found/i);
+    expect(linkElement).toBeInTheDocument();
+
+});
+
+test('renders Register button', () => {
+    render(
+        <MemoryRouter>
+            <LoginRegisterSwitch />
+        </MemoryRouter>
+    );
+
+    const buttonElement = screen.getByText(/Register/i);
+    expect(buttonElement).toBeInTheDocument();
 });
